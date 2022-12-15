164ca83279ba38bcc833901665679934dd3e386a
aaxsh <aamirshakir0@gmail.com>
2022-12-15 11:45:19 +0100

    adding sample plot

--- docs/src/routes/+page.svelte
+++ docs/src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script>
     import Example from "../content/example.md";
     import Video from "../content/Video.svelte";
+
+    import Plot from "svelte-plotly.js";
+
+    const data = [
+        {
+            x: [1, 2, 3, 4, 5],
+            y: [1, 2, 4, 8, 16],
+        },
+    ];
 </script>
 
 <Video />
@@ -8,6 +17,14 @@
 <div class="max-width-4 mx-auto" id="start">
     <h1>LOL</h1>
     <Example />
+    <Plot
+        {data}
+        layout={{
+            margin: { t: 0 },
+        }}
+        fillParent="width"
+        debounce={250}
+    />
 </div>
 
 <style>
